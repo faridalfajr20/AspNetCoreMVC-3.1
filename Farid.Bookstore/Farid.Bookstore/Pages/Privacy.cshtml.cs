89bf736bfc73f057c20d4c803215fa238f3bec24
faridalfajr20 <faridalfajr20@gmail.com>
2022-10-31 16:54:14 +0700

    Test commit and push

---
+++ Farid.Bookstore/Farid.Bookstore/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Farid.Bookstore.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
